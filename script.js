3c4ec7286dfe0c4846264bc84f6bb8fb4f73dbdc
Tim Östlund <timander116@gmail.com>
2019-10-02 13:12:40 +0200

    Börjat smått med första på javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+function svar(){
+    var total = 10;
+    var poäng = 0;
+    var F1 = document.forms["frågor"]["F1"].value;
+    var F2 = document.forms["frågor"]["F2"].value;
+    var F3 = document.forms["frågor"]["F3"].value;
+    var F4 = document.forms["frågor"]["F4"].value;
+    var F5 = document.forms["frågor"]["F5"].value;
+    var F6 = document.forms["frågor"]["F6"].value;
+    var F7 = document.forms["frågor"]["F7"].value;
+    var F8 = document.forms["frågor"]["F8"].value;
+    var F9 = document.forms["frågor"]["F9"].value;
+    var F10 = document.forms["frågor"]["F10"].value;
+    
+    alert(F1);
+}
